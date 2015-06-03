19ee85de17b6590e92e61d6460f491ada3e09e51
Rafael Calvillo Sanchez <Rafael@Mac-RCS.local>
2015-06-03 12:38:31 +0200

    Initial Commit

---
+++ lib_login/lib_login.h
@@ -0,0 +1,13 @@
+//
+//  lib_login.h
+//  lib_login
+//
+//  Created by Rafael Calvillo Sanchez on 3/6/15.
+//  Copyright (c) 2015 Rafael Calvillo Sanchez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface lib_login : NSObject
+
+@end
---
+++ lib_login/lib_login.m
@@ -0,0 +1,13 @@
+//
+//  lib_login.m
+//  lib_login
+//
+//  Created by Rafael Calvillo Sanchez on 3/6/15.
+//  Copyright (c) 2015 Rafael Calvillo Sanchez. All rights reserved.
+//
+
+#import "lib_login.h"
+
+@implementation lib_login
+
+@end
