582863cd1897d4b741fd34775c45a279eec5438a
Rafael Calvillo Sanchez <Rafael@Mac-RCS.local>
2015-06-05 08:38:34 +0200

    primer commit libreria

---
+++ lib_login/UserPass.h
@@ -0,0 +1,19 @@
+//
+//  UserPass.h
+//  usuarios
+//
+//  Created by Rafael Calvillo Sanchez on 1/6/15.
+//  Copyright (c) 2015 Rafael Calvillo Sanchez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserPass : NSObject
+
+@property(nonatomic, strong)NSString *user;
+@property(nonatomic, strong)NSString *pass;
+
+
+
+
+@end
---
+++ lib_login/UserPass.m
@@ -0,0 +1,18 @@
+//
+//  UserPass.m
+//  usuarios
+//
+//  Created by Rafael Calvillo Sanchez on 1/6/15.
+//  Copyright (c) 2015 Rafael Calvillo Sanchez. All rights reserved.
+//
+
+#import "UserPass.h"
+
+@implementation UserPass
+
+@synthesize user;
+@synthesize pass;
+
+
+
+@end
